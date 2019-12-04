1031c16fb27e13a822da84d8d2f0cd6895153731
indiependente <rockerg991@gmail.com>
2019-12-04 15:47:25 +0000

    day 1

---
+++ 1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	var fuelTotal float64
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		mass, err := strconv.Atoi(s.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fuelTotal += calculateFuel(float64(mass))
+	}
+
+	fmt.Printf("Total fuel required: %.2f\n", fuelTotal)
+}
+
+func calculateFuel(x float64) float64 {
+	f := math.Floor(x/3) - 2
+	if f < 0 {
+		return 0
+	}
+	return f + calculateFuel(f)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/indiependente/aoc2019
+
+go 1.13
